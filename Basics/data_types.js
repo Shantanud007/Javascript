c1c025d888a40b4e543c8d90e59c72818a0d67d3
Shantanud007 <44708965+Shantanud007@users.noreply.github.com>
2025-07-07 07:30:13 +0000

    #

---
+++ Basics/data_types.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using node.js , not browser
+
+let name = "shantanu"
+let age = 30
+
+// number 
+// bigint 
+// string=> "" 
+// boolean=> true/false
+// null=> standalone value 
+//undefined =>
+// symbol=> unique 
+
+
+//object 
+
+console.log(typeof null);
+console.log(typeof age);
