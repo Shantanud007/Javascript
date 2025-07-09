32e3d43493551f0b7cb68aba01bade8822879e9c
Shantanud007 <44708965+Shantanud007@users.noreply.github.com>
2025-07-09 18:10:25 +0000

    #

--- Basics/String.js
+++ Basics/String.js
@@ -3,7 +3,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('SD')
+const gameName = new String('Shantanu-d')
 
 console.log(gameName[0]);
 console.log(gameName.length);
@@ -13,6 +13,10 @@ console.log(gameName.__proto__);
 console.log(gameName.toLowerCase());
 console.log(gameName.toUpperCase());
 // console.log(gameName.toLocaleLowerCase);
+
+console.log(gameName.charAt(5));    
+console.log(gameName.indexOf('t'));
+
 
 
 
