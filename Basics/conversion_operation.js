2e2f89e6c375bcbb74d282471037c6487f4cbc05
Shantanud007 <44708965+Shantanud007@users.noreply.github.com>
2025-07-09 10:51:20 +0000

    #

---
+++ Basics/conversion_operation.js
@@ -0,0 +1,33 @@
+let a=10
+let b= "asd"
+
+let valueInNumber = Number (b)
+let valueInString = String (a)
+
+console.log(typeof valueInNumber);
+console.log(typeof valueInString);
+console.log(valueInNumber);
+console.log(valueInString);
+
+// "33"=> 33
+//"33asd"=>NaN
+// true=> 1 false=>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true 0=>flase
+//""=> flase
+//"anyvalue"=>ture
+
+const heros = ["shktima","nagraj","ironman "];
+let objheros={
+    name:"SD",
+    age: 27,
+}
+
+const myFunction= function(){
+    console.log("Hello");
+    
+}
