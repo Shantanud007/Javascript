5be7e47176c5b7bf18073d0da2dfdf8b767feaae
Shantanud007 <44708965+Shantanud007@users.noreply.github.com>
2025-07-11 19:49:14 +0000

    #

---
+++ Array/array.js
@@ -0,0 +1,36 @@
+// const myarr=[0,1,2,3,4,5,6]
+
+// // console.log(myarr[3]);
+
+// //Array Methods
+
+// const arr = [3,4,2,5,76,43,33,3,8]
+// // arr.push(33) 
+// // // arr.pop()
+// // arr.shift(0)
+// // console.log(arr.includes(100));
+// // console.log(arr.indexOf(100));
+// const arr1 = arr.join()
+
+// console.log(arr);
+// console.log(arr1);
+
+// // slice , splice
+// console.log("A", arr);
+// const arr2 = arr.slice(2, 4)
+// console.log(arr2);
+// const arr3= arr.splice(2,4)
+// console.log("B", arr);
+// console.log("C", arr);
+// console.log(arr3);
+
+console.log(Array.isArray("Shantanu"));
+console.log(Array.from("SHANTANU"));
+console.log(Array.from({name:"SHANTANU"}));
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
+
