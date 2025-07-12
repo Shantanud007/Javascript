7876c65822b92a60a53207396f0eb5ab23cc0ff4
Shantanud007 <44708965+Shantanud007@users.noreply.github.com>
2025-07-12 18:44:47 +0000

    #

---
+++ Array/obj.js
@@ -0,0 +1,15 @@
+const obj ={
+     name: "Shantanu",
+     email: "shantanud430@gmail.com",
+     Number: 1234567890,
+     [Symbol]: "key",
+     Add: "Pune"
+}
+console.log(obj[Symbol]);
+// console.log(obj.Symbol);
+console.log(obj.name);
+console.log(`Hello I'm ${obj.name}`);
+
+
+
+
